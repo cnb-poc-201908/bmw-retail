d89a458a03ba739d3a403bdcf883c0488165c771
liuqdl <liuqdl@cn.ibm.com>
2019-08-08 15:55:13 +0800

    change swagger enable

--- src/main/java/com/bmw/config/SwaggerConfig.java
+++ src/main/java/com/bmw/config/SwaggerConfig.java
@@ -49,8 +49,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
     //是否开启swagger，正式环境一般是需要关闭的，可根据springboot的多环境配置进行设置
-    @Value(value = "${swagger.enabled}")
-    Boolean swaggerEnabled;
+    //@Value(value = "${swagger.enabled}")
+    Boolean swaggerEnabled  = true;
 
     @Bean
     public Docket createRestApi() {
